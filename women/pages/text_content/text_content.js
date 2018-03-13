4d2289e27bee8451541442c0906dca727ffbfa46
xinghua <854592400@qq.com>
2018-03-13 15:45:11 +0800

    编写了文章列表及内容页

--- women/pages/text_content/text_content.js
+++ women/pages/text_content/text_content.js
@@ -5,58 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    
+    id: 2,
+    title: "软文，方法是“软“，精髓在”文",
+    img_src: "/images/content/yf2.jpg",
+    content: "然而，拨开层层有关软文的迷雾，最重要的还是那句话，“思路决定出路“，文笔和语言的表达是可以通过后天的勤奋学习锻炼出来的，然而一个清晰的思路才是保证出产优秀作品的基石，有了思路，才有目标、方法和技术等等这些必不可缺的软文要素。"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log(options.id);
+    // 在这里通过id获取对应的数据
+    this.setData({
+      id: options.id
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
   /**
    * 用户点击右上角分享
    */
--- women/pages/text_list/text_list.js
+++ women/pages/text_list/text_list.js
@@ -5,18 +5,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list : [
-      {
-        title: "白色滴美女衣服，好看哇",
-        img_src: "http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=a2160861871001e95a311c4cd0671199/3801213fb80e7bec553d4269252eb9389a506b56.jpg",
-        content: "123"
-      },{
-        title: "白色滴美女衣服，好看哇",
-        img_src: "http://imgsrc.baidu.com/image/c0%3Dshijue1%2C0%2C0%2C294%2C40/sign=a2160861871001e95a311c4cd0671199/3801213fb80e7bec553d4269252eb9389a506b56.jpg",
-        content: "456"
-      }
-      
-    ]
   },
   /**
    * 生命周期函数--监听页面加载
@@ -25,5 +13,12 @@ Page({
     this.setData({
       text_list: text_list.text_list
     });
+  },
+  toContent : function(event){
+    var id = event.currentTarget.dataset.id;
+    // console.log(id);
+    wx.navigateTo({
+      url: '../text_content/text_content?id='+id
+    })
   }
 })
