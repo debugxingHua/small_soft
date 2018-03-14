99e82dc352e825e6b1cb5d8c848f6c291f9e66a1
xinghua <854592400@qq.com>
2018-03-14 16:45:10 +0800

    文章实现接口调数据

--- women/pages/text_content/text_content.js
+++ women/pages/text_content/text_content.js
@@ -1,26 +1,33 @@
-// pages/text_content/text_content.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    
-    id: 2,
-    title: "软文，方法是“软“，精髓在”文",
-    img_src: "/images/content/yf2.jpg",
-    content: "然而，拨开层层有关软文的迷雾，最重要的还是那句话，“思路决定出路“，文笔和语言的表达是可以通过后天的勤奋学习锻炼出来的，然而一个清晰的思路才是保证出产优秀作品的基石，有了思路，才有目标、方法和技术等等这些必不可缺的软文要素。"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options.id);
-    // 在这里通过id获取对应的数据
-    this.setData({
-      id: options.id
-    });
+    var that = this;
+    // 根据id请求数据
+    wx.request({
+      url: 'http://szhtdms.3whost.com.cn/admin.php?c=api&a=getArticleContent',
+      method: 'POST',
+      header:{
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data:{
+        id: options.id
+      },
+      success:function(res){
+        that.setData({
+          id: options.id,
+          title: options.title,
+          content: res.data.data.content
+        });
+      }
+    })
   },
   /**
    * 用户点击右上角分享
--- women/pages/text_list/text_list.js
+++ women/pages/text_list/text_list.js
@@ -1,4 +1,4 @@
-var text_list = require("../../datas/text_list.js");
+// var text_list = require("../../datas/text_list.js");
 Page({
 
   /**
@@ -10,15 +10,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      text_list: text_list.text_list
-    });
+    var that = this;
+    // 调用api,得到文章列表
+    wx.request({
+      url: 'http://szhtdms.3whost.com.cn/admin.php?c=api&a=indexData',
+      header :{
+        'content-type': 'application/json' // 默认值
+      },
+      success : function(res){
+        // console.log(res.data.data);
+        // 传入数据
+        that.setData({
+          text_list: res.data.data
+        });
+      }
+    })
   },
+  //id传到内容页面
   toContent : function(event){
     var id = event.currentTarget.dataset.id;
+    var title = event.currentTarget.dataset.title;
     // console.log(id);
     wx.navigateTo({
-      url: '../text_content/text_content?id='+id
+      url: '../text_content/text_content?id='+id+'&title='+title
     })
   }
 })
